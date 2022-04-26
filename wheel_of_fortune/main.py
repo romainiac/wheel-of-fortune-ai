62c8809b03f723d53ecb735068745f06875429fc
unknown <romanyefimets@gmail.com>
2022-04-25 22:32:56 -0700

    added pygame to check if rect is in another rect

--- wheel_of_fortune/main.py
+++ wheel_of_fortune/main.py
@@ -1,6 +1,7 @@
 import cv2
 #import numpy as np
 import functools
+import pygame 
 
 cap = cv2.VideoCapture('assets/video2.mp4')
 
@@ -11,6 +12,10 @@ class Tile:
         self.w = w;
         self.h = h;
         self.color = color;
+
+    def __eq__(self, other):
+        print(self.x == other.x and self.y == other.y)
+        return self.x == other.x and self.y == other.y
 
 # sort by coordinates
 # y coorindate comes first, if y is the same within an allowence, x comes first
@@ -29,17 +34,24 @@ def sort(a, b):
 
 # check to see if tile a is within tile b
 def isWithin(a, b):
-    a_top_left = (a.x, a.y)
-    a_top_right = (a.x + a.w, a.y)
-    b_bottom_left = (a.x, a.y + a.h)
-    c_bottom_right = (a.x + w, a.y + h)
+    rect_a = pygame.Rect(a.x,a.y,a.w,a.h)
+    rect_b = pygame.Rect(b.x,b.y,b.w,b.h)
+    return rect_b.contains(rect_a)
+    # if a_top_left[0] < b_top_left[0] or a_top_left[1] < b_top_left[1]: return False; # top left corner not within
+    # if a_top_right[0] > b_top_right[0] or a_top_right[1] < b_top_right[1]: return False; # top right corner not within
+    
+    # if a_bottom_left[0] < b_bottom_left[0] or a_bottom_left[1] > b_bottom_left[1]: return False; # bottom left corner not within
+    # if a_bottom_right[0] > b_bottom_right[0] or a_bottom_right[1] > b_bottom_right[1]: return False;
 
-    b_top_left = (a.x, a.y)
-    b_top_right = (a.x + a.w, a.y)
-    b_bottom_left = (a.x, a.y + a.h)
-    b_bottom_right = (a.x + w, a.y + h)
 
-    return False
+def isWithinRect(b, a):
+    x1,y1,x2,y2 = b.x, b.y, b.x + w, b.y + h;
+    x,y = a[0], a[1];
+    if (x > x1 and x < x2 and
+        y > y1 and y < y2) :
+        return True
+    else :
+        return False
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -92,22 +104,24 @@ while cap.isOpened():
     # this will handle the cases where letters are treated as boxes
     # since they are sorted, the ones the remove (if any) will be next to each other) so we can avoid n^2
     # we might also only want to check it if its a white tile 
+    to_remove = []
     for i in range(len(tiles)):
         cur_tile = tiles[i];
-        to_remove = []
 
         # check left
         if (i - 1 >= 0):
             left_tile = tiles[i - 1];
             if isWithin(left_tile, cur_tile) and left_tile.color == 'white' and cur_tile.color == 'white': 
-                to_remove.append(left_tile)
+                 to_remove.append(left_tile)
         #check right
         if (i + 1 < len(tiles)):
             right_tile = tiles[i + 1];
             if isWithin(right_tile, cur_tile) and right_tile.color == 'white' and cur_tile.color == 'white': 
                 to_remove.append(right_tile)
 
-        for tile in to_remove: tiles.remove(tile)
+    for tile in to_remove: 
+        if tile in tiles:
+            tiles.remove(tile)
 
 
     for i in range(len(tiles)):
@@ -116,8 +130,10 @@ while cap.isOpened():
         if tile.color == 'green': 
             box_color = (0,255,255) # yellow
 
-        cv2.rectangle(with_contours, (tile.x, tile.y), (tile.x + tile.w, tile.y + tile.h), box_color, 2)
-        cv2.putText(with_contours, str(i), (tile.x, tile.y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.rectangle(with_contours, (tile.x, tile.y), (tile.x + tile.w, tile.y + tile.h), box_color, 1)
+        #cv2.putText(with_contours, str(i), (tile.x, tile.y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(with_contours, str(tile.y), (tile.x, tile.y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+
 
     words = ""
     for tile in tiles:
